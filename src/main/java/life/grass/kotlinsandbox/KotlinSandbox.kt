ef2d4bab2d2e1b190b9e7e3580be065c1cd58402
BlackBracken <me@bracken.black>
2019-04-12 01:26:48 +0900

    initialize project

---
+++ src/main/java/life/grass/kotlinsandbox/KotlinSandbox.kt
@@ -0,0 +1,16 @@
+package life.grass.kotlinsandbox
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author BlackBracken
+ */
+class KotlinSandbox : JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+
+        println("Hello, World!")
+    }
+
+}
